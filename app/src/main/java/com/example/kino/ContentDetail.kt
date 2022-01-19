f0c74149036e51d7fbdf4f2bba740624690740ca
akirsanov <akirsanov@akirsanovs-MacBook-Air.local>
2022-01-19 12:48:33 +0700

    init

---
+++ app/src/main/java/com/example/kino/ContentDetail.kt
@@ -0,0 +1,11 @@
+package com.example.kino
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContentDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_detail)
+    }
+}
