f3be0f88d8064eb2d5cd8c09d40c96b9571022ae
akirsanov <akirsanov@akirsanovs-MacBook-Air.local>
2022-03-21 21:21:47 +0700

    feat: update

--- app/src/main/java/com/example/kino/content_recycler/ContentItemViewHolder.kt
+++ app/src/main/java/com/example/kino/content_recycler/ContentItemViewHolder.kt
@@ -1,39 +1,24 @@
 package com.example.kino.content_recycler
 
-import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
-import coil.request.ImageRequest
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.model.GlideUrl
-import com.bumptech.glide.load.model.Headers
-import com.bumptech.glide.load.model.LazyHeaders
 import com.example.kino.R
-import com.example.kino.models.Content
 import com.example.kino.models.Movie
-// import com.example.kino.models.Playlist
-// import com.example.kino.models.Station
 import com.example.kino.models.posterUrl
 
 class ContentItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    private val name = itemView.findViewById<TextView>(R.id.title)
-    private val details = itemView.findViewById<TextView>(R.id.button)
+    private val title = itemView.findViewById<TextView>(R.id.title)
     private val description = itemView.findViewById<TextView>(R.id.description)
     private val image = itemView.findViewById<ImageView>(R.id.image)
-    private val favorite = itemView.findViewById<ImageView>(R.id.favorite)
+    private val buttonDetail = itemView.findViewById<TextView>(R.id.button)
+    private val buttonFavorite = itemView.findViewById<ImageView>(R.id.favorite)
 
     fun bind(item: Movie, listener: ContentItemAdapter.ContentClickListener) {
-        // if (item.isFavorite) {
-        //     favorite.setColorFilter(Color.RED)
-        // } else {
-        //     favorite.setColorFilter(Color.GRAY)
-        // }
-
-        name.apply {
+        title.apply {
             text = item.title
         }
 
@@ -42,43 +27,16 @@ class ContentItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) 
             maxLines = 2
         }
 
-        // image.load(item.images[0].url) {
-        //     headers(Headers("apikey", "Njc0NWI0MzEtZWQyOS00ZGY2LTgzYjQtM2FmOTYxNWUyMzNk")).build()
-        // }
-
-        // val request = ImageRequest.Builder(image.context)
-        //     .data(item.images[0].url)
-        //     // .setHeader("Cache-Control", "max-age=31536000")
-        //     .target(image)
-        //     .build()
-        // imageLoader.execute(request)
-
-        // val glideUrl = GlideUrl(
-        //     item.images[0].url, LazyHeaders.Builder()
-        //         .addHeader("apikey", "Njc0NWI0MzEtZWQyOS00ZGY2LTgzYjQtM2FmOTYxNWUyMzNk").build()
-        // )
         Glide.with(image.context)
             .load(item.posterUrl)
             .centerCrop()
             .into(image)
-        // Glide.with(image.context)
-        //     .load(glideUrl)
-        //     .into(image)
 
-        // Glide.with(image.context)
-        //     .load("${item.images[0].url}/?apikey=Njc0NWI0MzEtZWQyOS00ZGY2LTgzYjQtM2FmOTYxNWUyMzNk")
-        //     .centerCrop()
-        //     .into(image)
-
-        image.apply {
-            // setImageResource("item.poster")
-        }
-
-        details.setOnClickListener {
+        buttonDetail.setOnClickListener {
             listener.onClickDetails(item, adapterPosition)
         }
 
-        favorite.setOnClickListener {
+        buttonFavorite.setOnClickListener {
             listener.onClickFavorite(item, adapterPosition)
         }
     }
